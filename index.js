2c534750893ea9c038c090dbfc71f3f4c1d79364
Mathyas <mathyasautin@yahoo.com>
2024-09-20 15:49:43 -0300

    carousel finished, dots added

--- index.js
+++ index.js
@@ -1,67 +1,57 @@
-const allArrows = document.querySelectorAll('.arrow');
-const frame = document.querySelector('.frame');
+const control = document.querySelectorAll('.arrow');
 const slide = document.querySelector('.slide');
-const carouselImg = document.querySelectorAll('.carousel-img');
+const dots = document.querySelectorAll('.dot');
+const dotContainer = document.querySelector('.dots');
 
-let initial = 0;
-let amount = 840;
-// let index = 1;
-// let offset;
-
-// const nextRightImg = () => {
-//      initial -= amount;
-//     slide.style.transform = `translate(${initial}px)`;
-    
-// }
-// const nextLeftImg = () => {
-//     initial += amount;
-//    slide.style.transform = `translate(${initial}px)`;
-   
-// }
 
 const nextSlide = (e) => {
     let offset = e.target.classList.contains('right') === true ? 1 : -1;
-    // e.target.classList.contains('left') === true ? initial += amount : initial -= amount;
-    // offset == "1" ? initial += amount : initial -= amount;
     activeSlide(offset);
-    // slide.style.transform = `translate(${initial}px)`;
-    // slide.children[4].dataset.active === true ? loopSlide() : activeSlide(offset);
+    activeDot();
 }
 
 const activeSlide = (value) => {
-    let activeOne = slide.querySelector('[data-active]');
-    let nextIndex = [...slide.children].indexOf(activeOne) + value;
-    // let nextImg = slide.querySelector(`[data-index = "${index += value}"]`);
+    let activeImg = slide.querySelector('[data-active]');
+    let nextIndex = [...slide.children].indexOf(activeImg) + value;
 
     if (nextIndex < 0) {
         nextIndex = slide.children.length -1;
-        initial = -3360;
     }
     if (nextIndex >= slide.children.length) {
         nextIndex = 0
-        initial = 0;
     }
-    console.log(nextIndex);
 
     let nextImg = slide.children[nextIndex];
-    activeOne.removeAttribute('data-active');
+
+    activeImg.removeAttribute('data-active');
     nextImg.dataset.active = true;
-    
 }
 
-const loopSlide = () => {
-    console.log('oi');
-    slide.children[4].removeAttribute('data-active');
-    slide.children[0].dataset.active = true;
-    initial = 0;
-    slide.style.transform = `translate(${initial}px)`;
-    
+const selectSlide = (e) => {
+    let index = e.target.dataset.index;    
+    let showImg = slide.children[index -1];
+    slide.querySelector('[data-active]').removeAttribute('data-active');
+    showImg.dataset.active = true;
+    activeDot();
 }
 
-allArrows.forEach(arrow => {
+const activeDot = () => {
+    dots.forEach(dot => {
+        dot.classList.remove('active');
+    });
+
+    let activeIndex = [...slide.children].indexOf(slide.querySelector('[data-active]')) + 1 ;
+    let activeD = dotContainer.querySelector(`[data-index = '${activeIndex}']`);
+
+    activeD.classList.add('active');
+}
+
+
+control.forEach(arrow => {
     arrow.addEventListener("click",nextSlide)
 })
 
+dots.forEach(dot => {
+    dot.addEventListener('click', selectSlide)
+})
 
-// rightArrow.addEventListener('click', nextRightImg);
-// leftArrow.addEventListener('click', nextLeftImg);
